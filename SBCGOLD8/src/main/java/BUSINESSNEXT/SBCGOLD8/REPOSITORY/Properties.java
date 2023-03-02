ac57463032245abfce7989d4a8f4f9b3107e2316
rakassar <115995081+rakassar@users.noreply.github.com>
2023-03-02 13:19:35 +0530

    Update Properties.java

--- SBCGOLD8/src/main/java/BUSINESSNEXT/SBCGOLD8/REPOSITORY/Properties.java
+++ SBCGOLD8/src/main/java/BUSINESSNEXT/SBCGOLD8/REPOSITORY/Properties.java
@@ -1,11 +1,11 @@
 package BUSINESSNEXT.SBCGOLD8.REPOSITORY;
 
         public interface Properties {
-        String ChromeDriverPath= "E:\\Rizwan\\Automation\\lib\\chromedriver.exe";
+        String ChromeDriverPath= "D:\\Rizwan\\Automation\\lib\\chromedriver.exe";
 		String ChromeWebDriver = "webdriver.chrome.driver";
 		String URL= "https://sbcdev.crmnext.com/sag7/app/login/login";
-		String ExcelInputPath= "E:\\Rizwan\\Automation\\lib\\Data_Read_From_Excel\\SBC\\SBC_Datafromexcel.xlsx";
-		String pots= "E:\\Rizwan\\Automation\\lib\\Pots\\SBC\\";
-		String CSV= "E:\\Rizwan\\Automation\\lib\\Data_Read_From_Excel\\SBC\\Payroll.csv";
+		String ExcelInputPath= "D:\\Rizwan\\Automation\\lib\\Data_Read_From_Excel\\SBC\\SBC_Datafromexcel.xlsx";
+		String pots= "D:\\Rizwan\\Automation\\lib\\Pots\\SBC\\";
+		String CSV= "D:\\Rizwan\\Automation\\lib\\Data_Read_From_Excel\\SBC\\Payroll.csv";
         public static String WSDLUrl = "https://sbcdev.crmnext.com/accg7/CRMnextWebApi/CRMnextService.svc";
 		}
