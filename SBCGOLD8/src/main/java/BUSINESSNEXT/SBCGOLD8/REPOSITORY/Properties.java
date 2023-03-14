aa84bc109b221af542fd24949fc4b4ef471348d6
Somya <Somya@Somya_SAG>
2023-03-14 13:10:16 +0530

    Done

--- SBCGOLD8/src/main/java/BUSINESSNEXT/SBCGOLD8/REPOSITORY/Properties.java
+++ SBCGOLD8/src/main/java/BUSINESSNEXT/SBCGOLD8/REPOSITORY/Properties.java
@@ -1,8 +1,8 @@
 package BUSINESSNEXT.SBCGOLD8.REPOSITORY;
 
         public interface Properties {
-        String ChromeDriverPath= "D:\\Rizwan\\Automation\\lib\\chromedriver.exe";
-		String ChromeWebDriver = "webdriver.chrome.driver";
+       // String ChromeDriverPath= "D:\\Rizwan\\Automation\\lib\\chromedriver.exe";
+		//sString ChromeWebDriver = "webdriver.chrome.driver";
 		String URL= "https://sbcdev.crmnext.com/accg7/app/login/login";
 		String ExcelInputPath= "D:\\Rizwan\\Automation\\lib\\Data_Read_From_Excel\\SBC\\SBC_Datafromexcel.xlsx";
 		String pots= "D:\\Rizwan\\Automation\\lib\\Pots\\SBC\\";
